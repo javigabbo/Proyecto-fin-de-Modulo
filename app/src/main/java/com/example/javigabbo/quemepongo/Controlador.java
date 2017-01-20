f9d8932e80a60360f29306f1d8ab5f3da4c295d5
javigabbo <javigabbo@gmail.com>
2017-01-20 17:39:42 +0100

    Ya funciona correctamente el registro. He creado otra activity con un sidebarMenu.

--- app/src/main/java/com/example/javigabbo/quemepongo/Controlador.java
+++ app/src/main/java/com/example/javigabbo/quemepongo/Controlador.java
@@ -20,11 +20,11 @@ import java.util.concurrent.Executor;
 public class Controlador implements View.OnClickListener{
 
     MainActivity vista;
-    //Intent intent = null;
+    Intent intent = null;
 
     public Controlador(MainActivity view){
         this.vista = view;
-        //intent = new Intent(vista, Activity2.class)
+        intent = new Intent(vista, MenuActivity.class);
     }
 
     @Override
@@ -45,6 +45,13 @@ public class Controlador implements View.OnClickListener{
         }
     }
 
+
+
+    public void iniciarSesion(String email, String password){}
+
+
+
+
     public void registrarse(String email, String password){
 
         if(TextUtils.isEmpty(email)){
@@ -58,15 +65,21 @@ public class Controlador implements View.OnClickListener{
 
         vista.progressDialog.setMessage("Registrando usuario...");
         vista.progressDialog.show();
-        vista.firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
+        vista.firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(vista, new OnCompleteListener<AuthResult>() {
             @Override
-            public void onComplete(Task<AuthResult> task) {
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
                 if (task.isSuccessful()){
                     //Usuario se ha registrado con exito y ha iniciado sesion
+                    vista.progressDialog.hide();
                     Toast.makeText(vista, "¡Usuario registrado con éxito!", Toast.LENGTH_SHORT).show();
+                    vista.startActivity(intent);
+                    vista.finish();
                 }else {
+                    //Problema durante el registro
                     Toast.makeText(vista, "No se ha podido registrar el usuario, por favor inténtalo más tarde", Toast.LENGTH_LONG).show();
                 }
+
             }
         });
 
